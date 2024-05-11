b79311e34e153322ca52d80a0dcd943aa0370eed
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-11 17:10:09 +0100

    updated exercise level 3 folder by uncommenting solution 1

--- Day 10/Exercise Level 3/index.js
+++ Day 10/Exercise Level 3/index.js
@@ -1,28 +1,28 @@
 //solution 1
 
-// let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-// function userIdGeneratorByUser() {
-//     let generatedIds = '';
-//     let lengthOfCharacters = parseInt(prompt('Enter number of characters'));
-//     let numberOfIds = parseInt(prompt('Enter number of IDs to be generated'));
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function userIdGeneratorByUser() {
+    let generatedIds = '';
+    let lengthOfCharacters = parseInt(prompt('Enter number of characters'));
+    let numberOfIds = parseInt(prompt('Enter number of IDs to be generated'));
 
-//     if (isNaN(lengthOfCharacters) || isNaN(numberOfIds)) {
-//         alert('Please enter valid numbers');
-//         return;
-//     }
+    if (isNaN(lengthOfCharacters) || isNaN(numberOfIds)) {
+        alert('Please enter valid numbers');
+        return;
+    }
 
-//     for (let i = 0; i < numberOfIds; i++) {
-//         let userId = '';
-//         for (let j = 0; j < lengthOfCharacters; j++) {
-//             const randomId = Math.floor(Math.random() * characters.length);
-//             userId += characters[randomId];
-//         }
-//         generatedIds += userId + '\n';
-//     }
-//     console.log(generatedIds);
-// }
+    for (let i = 0; i < numberOfIds; i++) {
+        let userId = '';
+        for (let j = 0; j < lengthOfCharacters; j++) {
+            const randomId = Math.floor(Math.random() * characters.length);
+            userId += characters[randomId];
+        }
+        generatedIds += userId + '\n';
+    }
+    console.log(generatedIds);
+}
 
-// userIdGeneratorByUser();
+userIdGeneratorByUser();
 
 //solution 2
 
