76526dcdf73ad43db2d4d066213e1749ec303f25
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-11 20:38:53 +0100

    updated the file with my studies

---
+++ Day 11/index.js
@@ -0,0 +1,57 @@
+//window Global Object
+//scope.js
+a = 'JavaScript' // declaring a variable without let or const make it available in window object and this found anywhere
+b = 10 // this is a global scope variable and found in the window object
+function letsLearnScope() {
+  console.log(a, b)
+  if (true) {
+    console.log(a, b)
+  }
+}
+console.log(a, b) // accessible
+
+
+
+//scope.js
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+// Function scope
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  let value = false
+// block scope
+  if (true) {
+    // we can access from the function and outside the function but 
+    // variables declared inside the if will not be accessed outside the if block
+    let a = 'Python'
+    let b = 20
+    let c = 30
+    let d = 40
+    value = !value
+    console.log(a, b, c, value) // Python 20 30 true
+  }
+  // we can not access c because c's scope is only the if block
+  console.log(a, b, value) // JavaScript 10 true
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
+
+const userDetails = {
+    firstName: 'Morufat',
+    lastName: 'Lamidi',
+    age: 15,
+    country: 'Nigeria',
+    city: 'Kwara',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Tailwindcss',
+      'Bootstrap',
+      'Communication',
+      'Leadership'
+    ],
+    isMarried: false
+  }
+  console.log(userDetails)
