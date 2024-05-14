be8a0f5177cccbba40678dfb10be5a4c1d77a5e1
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-14 20:59:34 +0100

    updated day 14 folder

---
+++ Day 14/index.js
@@ -0,0 +1,21 @@
+// a callback function, the name of the function could be any name
+const callback = (n) => {
+    return n ** 2
+  }
+  // function that takes other function as a callback
+  function cube(callback, n) {
+    return callback(n) * n
+  }
+  console.log(cube(callback, 3))
+
+
+  const numbers = [3, 9, 3, 7]
+  const sumArray = arr => {
+  let sum = 0
+  arr.forEach(function(element) {
+    sum += element
+  })
+  return sum
+
+}
+console.log(sumArray(numbers))
