6a218837b8a8c2863c609d88ac494065db3bf1de
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-20 20:33:45 +0100

    created a folder for my day 20 study session

--- Day 16/Exercise Level 3/index.js
+++ Day 16/Exercise Level 3/index.js
@@ -61,6 +61,8 @@ const countries = [
   //solution 2
 
   
+
+  
   
   
   
---
+++ Day 20/index.js
@@ -0,0 +1,5 @@
+//destructuring arrays
+const myNames = ['Lamidi', 'Morufat', 'Kajogbola'];
+let [firstName, middleName, LastName] = myNames
+console.log(firstName, LastName)
+
