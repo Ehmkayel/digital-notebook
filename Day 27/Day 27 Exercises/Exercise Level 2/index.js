e0c8933bd1495123e0d41c1a043a0da0cfeccb22
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-27 17:38:28 +0100

    done with exercise level 2

---
+++ Day 27/Day 27 Exercises/Exercise Level 2/index.js
@@ -0,0 +1,14 @@
+//solution 1
+
+//this will log an error message since 10 is not greater than 20
+console.assert(10 > 2 * 10 )
+
+//solution 2
+
+//This will print the warn message to the console
+console.warn('Are you sure you want to submit your work ?, once submitted it cannot be reversed')
+
+//solution 3
+
+//this will print an error message to the console
+console.error('Your work cannot be reversed')
