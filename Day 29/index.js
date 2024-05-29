3a98aae7caad532f736ac30f6e1268ff704acb1a
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-29 15:55:35 +0100

    updated my day 29 folder

---
+++ Day 29/index.js
@@ -0,0 +1,6 @@
+//To define a class in JavaScript we need the keyword class , the name of a class in CamelCase and block code(two curly brackets). Let us create a class name Person.
+
+// syntax
+class ClassName {
+    //  code goes here
+}
