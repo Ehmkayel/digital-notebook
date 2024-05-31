15fbd81eaa5ef9fb457a6c66c398141ef2cb21d8
lamidimorufat <lamidimorufat0@gmail.com>
2024-05-31 18:02:18 +0100

    update day 31

---
+++ Day 31/Day 31 Exercises/Exercise Level 1/index.js
@@ -0,0 +1,41 @@
+//solution 1
+
+class Animal{
+    constructor(name, age, color, legs){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.legs = legs
+    }
+
+    animalDetails(){
+        console.log(`The name of my animal is ${this.name}, she is ${this.age} years old, she's ${this.color} in color and has ${this.legs} legs`)
+
+    }
+
+    get getDetails(){
+        return this.name
+    }
+
+    set setName(name){
+        this.name = name;   
+    }
+
+    getAnimalDetails(){
+        let name = this.animalDetails()
+        if(this.name.length > 4){
+            console.log('The name is greater than 4')
+        }
+    }
+}
+
+const animalInfo = new Animal('Bella', 6, 'Black', 4)
+const secondAnimal = new Animal('Layla', 3, 'White', 4)
+
+console.log(animalInfo)
+animalInfo.animalDetails()
+console.log(animalInfo.getDetails)
+secondAnimal.setName = 'Quartz';
+console.log(secondAnimal)
+animalInfo.getAnimalDetails()
+
