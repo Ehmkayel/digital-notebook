d4fd4ea8df4ec34c9da238c2f089b358fbffd9fb
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-04 19:02:11 +0100

    done with exercise level 1

---
+++ Day 35/Day 35 Exercises/Exercise Level 1/index.js
@@ -0,0 +1,8 @@
+//solution 1
+
+localStorage.setItem('firstName', 'Morufat')
+localStorage.setItem('lastName', 'Lamidi')
+localStorage.setItem('age', 20)
+localStorage.setItem('country', 'Nigeria')
+localStorage.setItem('city', 'Port-Harcourt')
+console.log(localStorage)
---
+++ Day 35/Day 35 Exercises/Exercise Level 1/readme.md
@@ -0,0 +1,3 @@
+## The Questions to Exercises: Level 1
+
+1. Store you first name, last name, age, country, city in your browser localStorage.
