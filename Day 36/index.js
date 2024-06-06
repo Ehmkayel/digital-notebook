a968e6b2a5507b4efcc31bb81af734b0f2312e5b
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-06 15:56:56 +0100

    update day 35

--- Day 36/index.js
+++ Day 36/index.js
@@ -21,3 +21,23 @@
         console.log(`second param ${data}`)
     }
   )
+
+  //call back Let us see a callback function which can take two parameters. The first parameter is err and the second is result. If the err parameter is false, there will not be error other wise it will return an error.
+
+  // Promise
+const doPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const skills = ['HTML', 'CSS', 'JS']
+      if (skills.length > 0) {
+        resolve(skills)
+      } else {
+        reject('Something wrong has happened')
+      }
+    }, 2000)
+  })
+  
+  doPromise
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.log(error))
