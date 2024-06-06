b90d85fc398f91cd6a36eed397cbd8c6cd8c1ef3
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-06 15:45:13 +0100

    updated day 36

--- Day 36/index.js
+++ Day 36/index.js
@@ -1,1 +1,23 @@
 //Promise.prototype.then() and Promise.prototype.catch() methods return promises, they can be chained.
+
+
+//The constructor syntax for a promise object is:
+// let promise = new Promise(function(resolve, reject) {
+//     // executor (the producing code, "singer")
+//   });
+
+  function getWeather(){
+    return new Promise(function(resolve, reject){
+        reject('sunny')
+
+    })
+  }
+  const promise = getWeather()
+  promise.then(
+    function(data){
+        console.log(`first Param ${data}`)
+    },
+    function(data){
+        console.log(`second param ${data}`)
+    }
+  )
