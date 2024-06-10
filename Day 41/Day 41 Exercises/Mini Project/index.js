ec7d834a82640a48999f74aa1e498f6913908dc6
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-10 17:40:04 +0100

    updated the mini project

--- Day 41/Day 41 Exercises/Mini Project/index.js
+++ Day 41/Day 41 Exercises/Mini Project/index.js
@@ -21,9 +21,30 @@ setInterval(() => {
 
 const subHeading = document.querySelector('h2')
 subHeading.style.textDecoration = 'underline'
-subHeading.style.fontSize = '16px'
+subHeading.style.fontSize = '18px'
 subHeading.style.fontWeight = 'normal'
 subHeading.style.margin = 'auto'
+
+const subHead = document.createElement('h3');
+const date = new Date();
+const dateString = date.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+const timeString = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
+const dateTime = `${dateString}, ${timeString}`;
+subHead.textContent = dateTime;
+subHead.style.width = '10%'
+subHead.style.margin = '10px auto 0'
+subHead.style.padding = '5px'
+subHead.style.fontSize = '14px'
+const ulElement = document.querySelector('ul');
+const parentElement = ulElement.parentNode;
+parentElement.insertBefore(subHead, ulElement);
+
+setInterval(() => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    subHead.style.backgroundColor = randomColor
+
+}, 1000)
+
 
 const lists = document.querySelectorAll('li')
 lists.forEach((list, i) => {
