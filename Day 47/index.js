fccff4d3180d3213473fd7282f1b6644a48bb8f1
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-17 21:06:52 +0100

    updated day 47 folder

---
+++ Day 47/index.js
@@ -0,0 +1,9 @@
+let title
+ for (let i = 0; i < 3; i++) {
+    title = document.createElement('h2')
+    title.className = 'title'
+    title.style.fontSize = '24px'
+    title.textContent = i
+    console.log(title)
+ }
+ 
