3468df226406c2bc9eda64b05eb220b2e5020fd5
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-20 18:11:52 +0100

    updated the index.js file

---
+++ Day 51/index.js
@@ -0,0 +1,20 @@
+// selectedElement.addEventListener('eventlistner', e => {
+//     // the activity you want to occur after the event will be in here
+//   })
+
+  //click: If we want to add an event listener to an element, we select the element first then attach the addEventListener(). This method takes event type and callback functions as argument.
+
+function getFullDetails(name, age, school){
+    console.log(`Name: ${name}, Age: ${age}, School: ${school}`);
+    
+}
+
+function showDetails(){
+    let name = "Ramadan El"
+    let age = 20
+    let school = "Green Fields Academy"
+    alert(`Name: ${name}, Age: ${age}, School: ${school}`)
+}
+
+
+
